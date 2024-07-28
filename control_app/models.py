52df95fcba6dd700df972ce373251cb3674a2246
Egaokira <egaokira@gmail.com>
2024-07-29 00:02:14 +0100

    c

--- control_app/models.py
+++ control_app/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
-# Create your models here.
 
 class LightSettings(models.Model):
-    color = models.CharField(max_length=7)  # hex color
+    color = models.CharField(max_length=7)  
     intensity = models.IntegerField()
     pattern = models.CharField(max_length=20)
 
